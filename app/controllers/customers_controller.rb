c490c2626deac9ed73512a8b0aafa028d7ad5bc5
kevinambrogi1 <kevinambrogi@hotmail.com>
2017-12-23 19:17:45 +0100

    finished chapter 6, ready to test

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,7 @@
 class CustomersController < ApplicationController
+
   PAGE_SIZE = 10
+
   def index
     @page = (params[:page] || 0).to_i
     if params[:keywords].present?
@@ -12,6 +14,12 @@ class CustomersController < ApplicationController
       offset(PAGE_SIZE * @page).limit(PAGE_SIZE)
     else
       @customers = []
+    end
+    respond_to do |format|
+      format.html {}
+      format.json {
+        render json: { customers: @customers }
+      }
     end
   end
 end
